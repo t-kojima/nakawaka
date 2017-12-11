1e621ca52c907f92492ee2330b50457ae4bb5b93
t-kojima <t-kojima@hokuichi-e.com>
2017-12-11 17:03:06 +0900

    add user name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:login]
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    p conditions
+    if login = conditions.delete(:login)
+      p "login"
+      where(conditions).where(['username = :value OR lower(email) = lower(:value)', { value: login }]).first
+    else
+      p "else login"
+      where(conditions).first
+    end
+  end
+
+  validates :username,
+            uniqueness: { case_sensitive: :false },
+            length: { minimum: 4, maximum: 20 }
 end
